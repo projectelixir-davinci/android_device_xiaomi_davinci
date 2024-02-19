48a968ec3ce5829ea2acab32ff9900f37dcc22aa
pure-soul-kk <krishnakripa34567@gmail.com>
2024-02-19 18:00:08 +0530

    davinci: init Project Elixir

--- aosp_davinci.mk
+++ aosp_davinci.mk
@@ -11,10 +11,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from davinci device
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Project Elixir stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_davinci
+# Elixir Officials
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+ELIXIR_BUILD_TYPE := OFFICIAL
+EXCLUDE_LAWNCHAIR := true
+
+PRODUCT_NAME := aosp_davinci
 PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9T
